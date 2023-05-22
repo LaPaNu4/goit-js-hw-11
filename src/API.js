d3323670c7137d89482dc7fc59da11c95ec1eb82
LaPaNu4 <120036931+LaPaNu4@users.noreply.github.com>
2023-05-23 01:57:45 +0300

    fill

--- src/API.js
+++ src/API.js
@@ -1,17 +1,20 @@
 const axios = require('axios').default;
 class APIService {
-  constructor() {
-    const API = 'https://pixabay.com/api/';
-    const API_KEY = '36628090-3939a6c61934d7181d416eca1';
-    this.value = value;
-    let page = 1;
+  constructor( ) {
+    this.API = 'https://pixabay.com/api/';
+    this.API_KEY = '36628090-3939a6c61934d7181d416eca1';
+    this.value ='';
+    this.page = 1;
   }
 
-  async fetchData(value) {
+  async fetchData() {
     const data = await axios.get(
-      `${API}?key=${API_KEY}&q=${this.value}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`
+      `${this.API}?key=${this.API_KEY}&q=${this.value}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40`
     );
+    return data.data.hits
   }
 }
 
 export default APIService;
+
+
--- src/index.js
+++ src/index.js
@@ -1,18 +1,74 @@
-/* <div class="photo-card">
-  <img src="" alt="" loading="lazy" />
+import APIService from './API.js';
+import Notiflix from 'notiflix';
+const apiService = new APIService();
+const refs = {
+  submitBtn: document.querySelector('.submit-btn'),
+  galleryEl: document.querySelector('.gallery'),
+  searchQ: document.querySelector('input'),
+  loadMorebnt: document.querySelector('.load-more'),
+};
+refs.submitBtn.addEventListener('click', newBtnClick);
+
+async function newBtnClick(e) {
+  e.preventDefault();
+  const value = refs.searchQ.value.trim();
+  if (value === '') {
+    return Notiflix.Notify.failure('Заповніть поле!');
+  }
+  apiService.value = value;
+  await apiService.fetchData(value).then(data => {
+    if (data.length === 0) {
+      return Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    }
+    refs.galleryEl.innerHTML = '';
+    markUp(data);
+    console.log(apiService.value);
+    refs.loadMorebnt.classList.remove('is-hidden');
+  });
+}
+
+refs.loadMorebnt.addEventListener('click', async e => {
+  try {
+    e.preventDefault();
+    apiService.page++;
+    
+    const data = await apiService.fetchData();
+    if (data.length === 0) {
+     
+    }
+    markUp(data);
+  } catch (err) {
+     return Notiflix.Notify.info(
+       "We're sorry, but you've reached the end of search results."
+     );;
+  }
+});
+
+function markUp(data) {
+  refs.galleryEl.insertAdjacentHTML(
+    'beforeend',
+    data
+      .map(({ webformatURL, tags, likes, views, comments, downloads }) => {
+        return `<div class="photo-card">
+  <img src="${webformatURL}" width="100%" height="200px" alt="${tags}" loading="lazy" />
   <div class="info">
     <p class="info-item">
-      <b>Likes</b>
+      Likes: ${likes}
     </p>
     <p class="info-item">
-      <b>Views</b>
+      Views: ${views}
     </p>
     <p class="info-item">
-      <b>Comments</b>
+      Comments: ${comments}
     </p>
     <p class="info-item">
-      <b>Downloads</b>
+      Downloads: ${downloads}
     </p>
   </div>
-</div>; */
-const refs = {};
+</div>`;
+      })
+      .join('')
+  );
+}
